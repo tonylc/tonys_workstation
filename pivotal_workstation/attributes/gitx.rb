9f3e3f7fe9d841a944ffd21aff50e6eaed54b4c0
tony <tony@songkick.com>
2011-10-22 13:30:43 +0100

    cleanup gitx

--- pivotal_workstation/attributes/gitx.rb
+++ pivotal_workstation/attributes/gitx.rb
@@ -1,1 +1,1 @@
-node.default["gitx_download_location"]="https://github.com/downloads/laullon/gitx/GitX-L_v0.8.3.zip"
+node.default["gitx_zip"]="https://github.com/downloads/laullon/gitx/GitX-L_v0.8.3.zip"
